686825f60f724351683706eac5308edab4353721
lubo08 <lubo08@hotmail.com>
2015-03-30 23:45:52 +0200

    Message resources update.

--- src/main/java/org/mylivedata/app/LiveChatApplication.java
+++ src/main/java/org/mylivedata/app/LiveChatApplication.java
@@ -3,6 +3,8 @@ package org.mylivedata.app;
 import java.sql.SQLException;
 
 import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,16 +12,18 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class LiveChatApplication {
-		
+
+    private static Logger logger = LoggerFactory.getLogger(LiveChatApplication.class);
+
 	public static void main(String[] args) throws Exception {
 		ApplicationContext ctx = SpringApplication.run(LiveChatApplication.class, args);
-		if(ctx.getEnvironment().getProperty("h2.console").equals("true")){
+        if(ctx.getEnvironment().getProperty("h2.console").equals("true")){
 			DriverManagerDataSource dm = (DriverManagerDataSource)ctx.getBean("dataSource");
 			try {
-				Server.startWebServer(dm.getConnection());
+                logger.info("Starting H2 console");
+                Server.startWebServer(dm.getConnection());
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+                logger.error(e.getMessage());
 			}
 		}
 	}
--- src/main/java/org/mylivedata/app/configuration/ApplicationCofiguration.java
+++ src/main/java/org/mylivedata/app/configuration/ApplicationCofiguration.java
@@ -9,10 +9,13 @@ import org.mylivedata.app.dashboard.repository.service.LayoutService;
 import org.mylivedata.app.util.SessionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.HttpStatus;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 
 @Configuration
@@ -41,21 +44,31 @@ public class ApplicationCofiguration {
 		return resolver;
 	}
 	
-//	@Bean
-//	public ReloadableResourceBundleMessageSource defaultMessageSource() {
-//		ReloadableResourceBundleMessageSource defaultMessageSource = new ReloadableResourceBundleMessageSource();
-//		defaultMessageSource.setBasename("messages");
-//		defaultMessageSource.setDefaultEncoding("utf-8");
-//		return defaultMessageSource;
-//	}
-//	
-//	
-//	@Bean
-//	public MessageSource messageSource() {
-//		DatabaseDrivenMessageSource dbMessageSource = new DatabaseDrivenMessageSource();
-//		dbMessageSource.setParentMessageSource(defaultMessageSource());
-//		return dbMessageSource;
-//	}
+	@Bean
+	public ReloadableResourceBundleMessageSource defaultMessageSource() {
+		ReloadableResourceBundleMessageSource defaultMessageSource = new ReloadableResourceBundleMessageSource();
+		defaultMessageSource.setBasename("classpath:/messages");
+		defaultMessageSource.setDefaultEncoding("utf-8");
+		return defaultMessageSource;
+	}
 	
+	
+	@Bean
+	public MessageSource messageSource() {
+		DatabaseDrivenMessageSource dbMessageSource = new DatabaseDrivenMessageSource();
+		dbMessageSource.setParentMessageSource(defaultMessageSource());
+		return dbMessageSource;
+	}
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return (container -> {
+            ErrorPage error403Page = new ErrorPage(HttpStatus.FORBIDDEN, "/error403");
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/error404");
+            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/customerror");
+            container.addErrorPages(error403Page, error404Page, error500Page);
+        });
+    }
+
 	
 }
